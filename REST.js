b5091956fc18297fe34056b1e938a0f0bd443552
milindl <imilindluthra@gmail.com>
2016-12-04 19:59:28 +0530

    Set up NPM things, complete REST of the work

--- REST.js
+++ REST.js
@@ -1,22 +1,44 @@
+/*
+* The REST API for SaveSortMovie
+*/
+
+// Fetch dependencies
 var express = require("express");
-var bodyParser = require("body-parser");
 var md5 = require("blueimp-md5");
 var getDuration = require("get-video-duration");
 var serveStatic = require("serve-static");
+var fileUpload = require('express-fileupload');
+var multer = require("multer");
+
+// Set up Multer
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+	cb(null, __dirname + '/video_store');
+    },
+    filename: function (req, file, cb) {
+	
+	cb(null, Date.now() + "-" + file.originalname);
+    }
+});
+var upload = multer({storage:storage });
+
+// Set up application and model
 var app; 
 var videoModel; 
 
 var init = function init(v) {
     app = express();
-    app.use(bodyParser.json());
+    
     videoModel = v;
     // Set up static file serving
     app.use(serveStatic(__dirname + '/client'));
     app.use(serveStatic(__dirname + '/video_store'));
 }
 
+// Mainloop
 var watch = function watch() {
-    app.get("/get_video", function getVideo(req, res) {
+    app.get("/get_video", upload.array(), function getVideo(req, res) {
+	// Get video from ID
 	var rId = req.query.id;
 	videoModel.find({id: rId},
 	    function getVideoCallback(err, videos) {
@@ -26,29 +48,64 @@ var watch = function watch() {
 		}
 		if (videos.length == 0) res.sendStatus(404);
 		else res.send(videos[0]);
+		console.log(videos[0]);
 	    });
     });
-    app.put("/add_video", function addVideo(req, res) {
-	var videoData = req.body;
-	var rString = "" + (Math.random());
-	videoData["id"] = md5(videoData.title + rString)
-	getDuration(__dirname + "/video_store/" + videoData.filepath).then(function (duration){
-	    videoData["length"] = duration;
-	    videoModel.create(videoData,
-		function addVideoCallback(err, addedVideo) {
-		    if(err) {
-			console.log(err);
-			res.sendStatus(503);
-		    }
-		    console.log(addedVideo);
-		    res.sendStatus(200);
-		    }
-		);
-	});
+    
+    app.post("/upload_video",
+	     upload.single("vid"),
+	     function addVideo(req, res) {
+		 // Save video to database
+		 var videoData = req.body;
+		 videoData.id = md5(req.file.filename);
+		 videoData.actors = [];
+		 videoData.tags = videoData.tags.split(" ");
+		 videoData.filepath = req.file.filename;
+		 getDuration(__dirname + "/video_store/" + videoData.filepath).then(function (duration){
+		     videoData["length"] = duration;
+		     videoModel
+			 .create(videoData,
+				 function addVideoCallback(err,addedVideo) {
+				     if(err) {
+					 console.log(err);
+					 res.sendStatus(503);
+				     }
+				     console.log(addedVideo);
+				     res.sendStatus(200);
+				 }
+				);
+		 });
+	     });
+
+    app.get("/search_videos",
+	    upload.array(),
+	    function searchVideo(req, res) {
+		videoModel.find({},
+				function svCallback(err,vids) {
+				    res.status(200);
+				    for(var i = 0; i != vids.length; i++) {
+					var id = vids[i].id;
+					var title = vids[i].title;
+					var actors = vids[i].actors;
+					var length = vids[i].length;
+					vids[i] = {};
+					vids[i] = {
+					    id: id,
+					    title: title,
+					    actors: actors,
+					    length: length
+					};
+				    }
+				    console.log(vids);
+				    res.end(JSON.stringify(vids));
+				}
+			       );
+	    });
+
+    app.get("/", upload.array(), function(req, res) {
+	res.sendFile('client/view.html', {root: __dirname });
     });
-    app.get("/", function(req, res) {
-	res.sendFile(__dirname + "/client/view.html");
-    });
+    // Start listening
     app.listen(6700);
 }
 
